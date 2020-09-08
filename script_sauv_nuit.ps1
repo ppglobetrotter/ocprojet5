c6011a102c0b586deb19534e7d4b37eaae41820e
Admin <admin@acme.fr>
2020-09-08 15:39:22 +0200

    Transfert du script sur GitHub

---
+++ script_sauv_nuit.ps1
@@ -0,0 +1,23 @@
+﻿#Script sauvegarde de nuit des postes clients
+
+Try {
+#Sélection des ordinateurs avec Windows 10 dessus.
+$Names = Get-ADComputer -Filter { OperatingSystem -Like '*Windows*10*' } -Properties OperatingSystem | Select -Expand Name
+
+}
+
+Catch{
+Write-Host "Impossible de trouver les ordinateurs"
+exit 1
+}
+
+#Boucle de copie des dossiers utilisateurs
+ForEach ($Name in $Names)
+{
+
+xcopy /S /I \\$Name\C$\Users C:\SAVE\$Name /Y >>C:\SAVE\LOG\sauvegarde.log
+
+
+}
+
+
